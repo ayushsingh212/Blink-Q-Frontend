c1666e3a32d8dc49e98fa4758861bc06a2b8f6a3
ayushsingh212 <ayurajput249@gmail.com>
2025-10-10 18:04:18 +0530

    Renamed the branch name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import VideoFeed from '../components/VideoFeed.jsx'
 export default function Home() {
 return (
 <div>
-<h2 className="text-2xl mb-4">Home</h2>
+<h2 className="text-2xl mb-4">Home ||</h2>
 <VideoFeed videos={videos} />
 </div>
 )
