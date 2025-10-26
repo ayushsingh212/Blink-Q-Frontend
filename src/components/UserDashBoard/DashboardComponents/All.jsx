37ccf4c58fd195d4e0a98d841dfa46094984047a
Ansh Tripathi <ansh.tripathi17012006@gmail.com>
2025-10-26 22:42:54 +0530

    Added the lobby btn

--- src/components/UserDashBoard/DashboardComponents/All.jsx
+++ src/components/UserDashBoard/DashboardComponents/All.jsx
@@ -2,14 +2,18 @@ import react from "react";
 import Image from "./AllComponents/Image";
 import UserDetails from "./AllComponents/UserDetails";
 import WatchHistoryAll from "./AllComponents/WatchHistoryAll";
+import { useNavigate } from "react-router-dom";
 
 export default function All({ onSelectCategory }) {
+
+    const navigate = useNavigate();
 
     return (
         <>
             <Image />
             <UserDetails onSelectCategory={onSelectCategory} />
             <WatchHistoryAll />
+            <button onClick={()=> navigate('/lobby')}>Lobby</button>
         </>
     )
 }
